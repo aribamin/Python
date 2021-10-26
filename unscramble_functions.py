9117ef824871752a9b1dba5fc149d35e4c6c806d
Arib Amin <arib_amin@yahoo.com>
2021-10-26 02:49:14 -0400

    Update unscramble_functions.py

--- unscramble_functions.py
+++ unscramble_functions.py
@@ -1,16 +1,3 @@
-"""CSC108/A08: Fall 2021 -- Assignment 1: unscramble
-
-This code is provided solely for the personal and private use of
-students taking the CSC108/CSCA08 course at the University of
-Toronto. Copying for purposes other than this use is expressly
-prohibited. All forms of distribution of this code, whether as given
-or with any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2021 Michelle Craig, Anya Tafliovich.
-
-"""
-
 # Valid moves in the game.
 SHIFT = 'S'
 SWAP = 'W'
